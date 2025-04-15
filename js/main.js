76b7404823c4652cecdbe8fbf7c61588a33aaec0
SamurayDev <gabriel.m.s2100@gmail.com>
2025-04-15 09:17:58 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#carousel-images").slick({
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+  });
+//   setTimeout(function() {
+//     $('.slick-next').text('>');
+//     $('.slick-prev').text('<');
+//   }, 100);
+});
